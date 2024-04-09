10aef214d90f883c4dc78233b675d1105eaed86b
cwy <2103370@1utar.my>
2024-04-09 20:12:30 +0800

    Update homepage link in package.json and remove unnecessary Bootstrap CSS and JS imports

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   
   return (
 
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <ScrollToTop />
       <Routes>
         <Route path="/" element={<Layout />}>
