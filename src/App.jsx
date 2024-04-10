69e83ffc891ed743f5cbd2f6471c5dd83c47a2f6
cwy <2103370@1utar.my>
2024-04-11 01:27:56 +0800

    upgrade to router provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,60 @@
 // Importing modules
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
 import Home from "./Home";
 import About from "./About";
-import Ethinicity from "./Ethinicity";
+import Ethnicity from "./Ethnicity";
 import Games from "./Games";
 import Food from "./Food";
 import Culture from "./Culture";
 import ContactUs from "./ContactUs";
 import NotFound from "./NotFound";
 import SignInSide from "./SignInSide";
-import ScrollToTop from "./reuseComponent/ScrollToTop";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const route = [
+  {
+    Component: Layout,
+    children: [
+      { path: "/", Component: Home },
+      { path: "/about", Component: About },
+      { path: "/ethnicity", Component: Ethnicity },
+      { path: "/games", Component: Games },
+      { path: "/food", Component: Food },
+      { path: "/culture", Component: Culture },
+      { path: "/contact-us", Component: ContactUs },
+      { path: "/login", Component: SignInSide },
+      { path: "*", Component: NotFound },
+    ],
+  },
+]
+
+const router = createBrowserRouter(
+  route,
+  {
+    basename: process.env.PUBLIC_URL,
+  }
+);
+
+// function Root () {
+//   return (
+//     <Routes>
+//         <Route path="about" element={<About />} />
+//         <Route path="ethnicity" element={<Ethnicity />} />
+//         <Route path="games" element={<Games />} />
+//         <Route path="food" element={<Food />} />
+//         <Route path="culture" element={<Culture />} />
+//         <Route path="contact-us" element={<ContactUs />} />
+//         <Route path="login" element={<SignInSide />}/>
+//       <Route path="*" element={<NotFound />} />
+//     </Routes>
+//   )
+// }
 
 function App() {
   
   return (
-
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="ethnicity" element={<Ethinicity />} />
-          <Route path="games" element={<Games />} />
-          <Route path="food" element={<Food />} />
-          <Route path="culture" element={<Culture />} />
-          <Route path="contact-us" element={<ContactUs />} />
-          <Route path="login" element={<SignInSide />}/>
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   );
 }
 
